f7a4a5b770e80fa860efbbe1ef8bf8aa8cee0941
namihaha77 <47521419+namihaha77@users.noreply.github.com>
2020-02-19 15:27:11 +0400

    feat: solve task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let result = '';
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      for (let i = 0; i < element.length; i += 1) {
+        if (element[i] !== ' ') {
+          result += element[i].toUpperCase();
+          break;
+        }
+      }
+    }
+  });
+
+  return result.split('').sort().join('');
+}
